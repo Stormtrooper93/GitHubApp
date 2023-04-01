42798c4f55aa9a7132b55b7a9454f9fa3e1a95d9
Stormtrooper93 <vvetall@gmail.com>
2023-04-01 18:54:19 -0400

    before upd

--- script.js
+++ script.js
@@ -2,94 +2,98 @@
 
 class Github {
     constructor() {
-        this.clientId = 'e19797f2d6b0e6ad8256';
-        this.clientSecret = '8839acbbff48629d1d79ceac1b5a21d015d6ba13';
+        this.clientId = '7bcb9287a947a15b2ba8';
+        this.clientSecret = 'ff7f513c529318c255a06675c735dfcd3fea2cad';
     }
 
     async getUser(userName) {
-        const data = await fetch(`https://api.github.com/user/${userName}?client_id='e19797f2d6b0e6ad8256'&client_secret='8839acbbff48629d1d79ceac1b5a21d015d6ba13'`);
+        const data = await fetch(`https://api.github.com/users/${userName}?client_id=${this.clientId}&client_secret=${this.clientSecret}`);
         const profile = await data.json();
         return profile;
+      }
+    
     }
-};
-
-class UI {
-    constructor() {
-        this.profile = document.querySelector(`.profile`);
-    }
-    showProfile(user) {
+    
+    class UI {
+      constructor() {
+        this.profile = document.querySelector('.profile');
+      }
+    
+      showProfile(user) {
         this.profile.innerHTML = `
-            <div class="card card-body mb-3">
+          <div class="card card-body mb-3">
             <div class="row">
-            <div class="col-md-3">
+              <div class="col-md-3">
                 <img class="img-fluid mb-2" src="${user.avatar_url}">
                 <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
-            </div>
-            <div class="col-md-9">
+              </div>
+              <div class="col-md-9">
                 <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
                 <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
                 <span class="badge badge-success">Followers: ${user.followers}</span>
                 <span class="badge badge-info">Following: ${user.following}</span>
                 <br><br>
                 <ul class="list-group">
-                <li class="list-group-item">Company: ${user.company}</li>
-                <li class="list-group-item">Website/Blog: ${user.blog}</li>
-                <li class="list-group-item">Location: ${user.location}</li>
-                <li class="list-group-item">Member Since: ${user.created_at}</li>
+                  <li class="list-group-item">Company: ${user.company}</li>
+                  <li class="list-group-item">Website/Blog: ${user.blog}</li>
+                  <li class="list-group-item">Location: ${user.location}</li>
+                  <li class="list-group-item">Member Since: ${user.created_at}</li>
                 </ul>
+              </div>
             </div>
-            </div>
-        </div>
-        <h3 class="page-heading mb-3">Latest Repos</h3>
-        <div class="repos"></div>
-        `
-    }
-    showAlert(message, className) {
-        this.clearAlert()
+          </div>
+          <h3 class="page-heading mb-3">Latest Repos</h3>
+          <div class="repos"></div>
+      }
+    
+      showAlert(message, className) {
+        this.clearAlert();
         const div = document.createElement('div');
         div.className = className;
         div.appendChild(document.createTextNode(message));
         const container = document.querySelector('.searchContainer');
         const search = document.querySelector('.search');
         container.insertBefore(div, search);
-
+    
         setTimeout(() => {
-            this.clearAlert();
-        }, 2000);
-    }
-    clearAlert() {
+          this.clearAlert();
+        }, 2000)
+      }
+    
+      clearAlert() {
         const alertBlock = document.querySelector('.alert');
         if(alertBlock) {
-            alertBlock.remove();
+          alertBlock.remove();
         }
-    }
-
-    clearProfile() {
-        this.profile.innerHTML = '';
-    }
-}
-
-const github = new Github;
-const ui = new UI;
-
-const searchUser = document.querySelector(`.searchUser`);
-
-searchUser.addEventListener(`keyup`, (e) => {
-    const userText = e.target.value;
+      }
     
-    if(userText.trim() !== '') {
-        github.getUser(userText)
-            .then(data => {
-                console.log(data.message)
-                if(data.message === 'Not Found'){
-                    //показувати помилку
-                    ui.showAlert('User not found', 'alert alert-danger');
-                } else {
-                    ui.showProfile(data);
-                }
-            })
-    } else {
-        //очистити інпут пошуку
-        ui.clearProfile();
+      clearProfile() {
+        this.profile.innerHTML = '';
+      }
     }
-})
+    
+    const github = new Github;
+    const ui = new UI;
+    
+    const searchUser = document.querySelector('.searchUser');
+    
+    searchUser.addEventListener('keydown', (e) => {
+      const userText = e.target.value;
+    
+      if(userText.trim() !== '') {
+        // setTimeout( () => {
+          github.getUser(userText)
+            .then(data => {
+              if(data.message === 'Not Found') {
+                // показувати помилку
+                ui.showAlert('User not found', 'alert alert-danger');
+              } else {
+                ui.showProfile(data);
+              }
+            })
+          // }, 1000)
+      } else {
+        // очистити інпут пошуку
+        ui.clearProfile();
+      }
+    })
